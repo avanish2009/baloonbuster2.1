368af8c66ebb61caad75f94a7363eb10e5d5d9c8
avanish2009 <88578674+avanish2009@users.noreply.github.com>
2021-09-12 14:32:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,171 @@
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+var arrowGroup,redballooonGroup
+var arrowGroup,pinkballooonGroup
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  pink_balloonImage = loadImage("pink_balloon0.png");
+  blue_balloonImage = loadImage("blue_balloon0.png");
+  green_balloonImage = loadImage("green_balloon0.png");
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //creating background
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // creating bow to shoot arrow
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+
+
+
+  arrowGroup=new Group()
+  redballooonGroup=new Group()
+
+  arrowGroup=new Group()
+  pinkballooonGroup=new Group()
+
+  arrowGroup=new Group()
+  blueballooonGroup=new Group()
+
+  arrowGroup=new Group()
+  greenballooonGroup=new Group()
+}
+
+function draw() {
+ background(0);
+  // moving ground
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //moving bow
+  bow.y = World.mouseY
+  
+   // release arrow when space key is pressed
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //creating continous balloons
+  var select_balloon = Math.round(random(1,4));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
+    }
+    else if (select_balloon == 2) {
+      blueBalloon();
+    }
+    else if (select_balloon == 3 ) {
+      greenBalloon();
+    }
+    else{
+      pinkBalloon()
+    }
+
+
+
+  }
+
+  if(arrowGroup.isTouching(redballooonGroup))
+  if(arrowGroup.isTouching(pinkballooonGroup))
+  if(arrowGroup.isTouching(blueballooonGroup))
+  if(arrowGroup.isTouching(greenballooonGroup))
+  
+
+  {
+
+    arrowGroup.destroyEach()
+    redballooonGroup.destroyEach()
+    score=score+2
+
+    arrowGroup.destroyEach()
+    pinkballooonGroup.destroyEach()
+    score=score+4
+
+    arrowGroup.destroyEach()
+    blueballooonGroup.destroyEach()
+    score=score+6
+
+    arrowGroup.destroyEach()
+    greenballooonGroup.destroyEach()
+    score=score+8
+
+  }
+  
+  drawSprites();
+}
+
+
+// Creating  arrows for bow
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+  arrowGroup.add(arrow)
+}
+
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+  redballooonGroup.add(red)
+}
+
+function blueBalloon() {
+  //write code for spwaning blue balloons
+  var blue = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  blue.addImage(blue_balloonImage);
+  blue.velocityX = 3;
+  blue.lifetime = 150;
+  blue.scale = 0.1;
+  blueballooonGroup.add(blue)
+
+}
+
+function greenBalloon() {
+  //write code for spwaning green balloons
+  var green = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  
+  green.addImage(green_balloonImage);
+ green.velocityX = 3;
+  greenlifetime = 150;
+  green.scale = 0.1;
+ greenballooonGroup.add(green)
+
+}
+
+function pinkBalloon() 
+{
+  //write code for spwaning pink balloons
+
+  var pink = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  pink.addImage(pink_balloonImage);
+ pink.velocityX = 3;
+  pink.lifetime = 150;
+  pink.scale = 0.8;
+  pinkballooonGroup.add(pink)
+
+}
